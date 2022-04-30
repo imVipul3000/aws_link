cbbffd0fa279f4b01765df5556840c1557902fe3
Vipul Joshi <joshivipul097@gmail.com>
2022-04-30 14:47:55 +0530

    working blog

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/vipulblog/blog
+
+cd /home/vipulblog/blog
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 #start our node app in the background
 node src/index.js
