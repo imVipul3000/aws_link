d9715ade3813f9b183aeb19da6be01c948e4d6e9
Vipul Joshi <joshivipul097@gmail.com>
2022-04-30 14:44:15 +0530

    working blog

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/vipulblog/blog
-
-#navigate into our working directory where we have all our github files
-cd /home/vipulblog/blog
-
 #start our node app in the background
-node scr/index.js > app.out.log 2> app.err.log < /dev/null & 
+node src/index.js
